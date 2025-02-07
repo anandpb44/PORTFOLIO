285f66d920d051fe9fc2c9655a202ceb392adf7e
anandpb44 <anandpb44@gmail.com>
2025-02-07 16:19:51 +0530

    progress

---
+++ static/js/script.js
@@ -0,0 +1,29 @@
+var tablinks = document.getElementsByClassName("tab-links")
+var tabcontents = document.getElementsByClassName("tab-contents")
+
+// function select(event,tabname) {
+
+//     for (tl of tablinks) {
+//                 tl.classList.remove("active-link");
+//         }
+//     for (tb of tabcontents) {
+//         tb.classList.remove("active-tab");
+//         }
+
+//     event.currentTarget.classList.add("active-link")
+//     document.getElementById(tabname).classList.add(" active-tab")
+// }
+
+function select(event, tabname) {
+    // Remove 'active' classes from all tabs and tab contents
+    Array.from(tablinks).forEach(function(tl) {
+        tl.classList.remove("active-link");
+    });
+    Array.from(tabcontents).forEach(function(tb) {
+        tb.classList.remove("active-tab");
+    });
+
+    // Add 'active' classes to the selected tab and its content
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
