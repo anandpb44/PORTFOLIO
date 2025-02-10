8851e2829b2dff94b9534fcce593504fc7aa75f1
anandpb44 <anandpb44@gmail.com>
2025-02-10 16:37:27 +0530

    progress

--- portfolio.py
+++ portfolio.py
@@ -1,9 +1,43 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,redirect,request,url_for
+from flask_mail import Mail, Message 
 app=Flask(__name__)
 
-@app.route('/index',method=['GET'])
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'anandpb44@gmail.com'
+app.config['MAIL_PASSWORD'] = 'waeo pnio aalc yvjt'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_DEFAULT_SENDER'] = 'your_email@gmail.com'
+mail = Mail(app) 
+
+
+@app.route('/index')
 def Index():
     return render_template('index.html')
+
+
+@app.route('/contact', methods=['POST'])
+def contact():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        message = request.form['message']
+
+        # Create the email message
+        msg = Message(
+            subject=f"{name} contacting you via portfolio",
+            recipients=['anandpb44@gmail.com'], 
+            body=f"Name: {name}\nEmail: {email}\nMessage: {message}"
+        )
+
+        try:
+            # Send the email
+            mail.send(msg)
+            return redirect(url_for('index'))  # Redirect to homepage after sending the message
+        except Exception as e:
+            print(f"Error sending email: {e}")
+            return "There was an error sending your message. Please try again later."
 
 
 if __name__ == '__main__':
--- static/js/script.js
+++ static/js/script.js
@@ -27,3 +27,13 @@ function select(event, tabname) {
     event.currentTarget.classList.add("active-link");
     document.getElementById(tabname).classList.add("active-tab");
 }
+
+
+
+var sidemenu=document.getElementById('menubar');
+function openmenu(){
+    sidemenu.style.right = "0";
+}
+function closemenu(){
+    sidemenu.style.right = " -200px ";
+}
